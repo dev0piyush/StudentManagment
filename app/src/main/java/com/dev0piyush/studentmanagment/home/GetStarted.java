6f995ff68226b5709ba40849d02e7110c5065ece
dev0piyush <dev0piyush@gmail.com>
2023-07-24 11:25:50 +0530

    Default Project

---
+++ app/src/main/java/com/dev0piyush/studentmanagment/home/GetStarted.java
@@ -0,0 +1,16 @@
+package com.dev0piyush.studentmanagment.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dev0piyush.studentmanagment.R;
+
+public class GetStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_started);
+    }
+}
